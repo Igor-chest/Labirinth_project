ee07d390ca38ce9dbebe40cc5de91600e3a8bba9
Igor-chest <55881908+Igor-chest@users.noreply.github.com>
2019-10-09 20:41:50 +0500

    Update add_points.py

--- add_points.py
+++ add_points.py
@@ -18,24 +18,24 @@ time = 50
 x = 20
 y = 20
 print(__doc__)
-delta =  30 * x * y // time               # очки, полученные игроком
+delta = 30 * x * y // time               # очки, полученные игроком
+
+file_name = 'point.txt'            # файл с текущими очками
+file_name2 = 'record.txt'          # файл с рекордными очками
+points = 0
+new_points = 0
+player = input('введите ник: ')      # никнейм игрока + очков
+changed_file = []
+changed_file2 = []
 
 def add_points():
-    file_name = 'point.txt'            # файл с текущими очками
-    file_name2 = 'record.txt'          # файл с рекордными очками
-    points = 0
-    new_points = 0
-    player = input('введите ник: ')      # никнейм игрока + очков
-    changed_file = []
-    changed_file2 = []
-
-    with open(file_name, 'r') as f:                           # ищем в файле очки игрока
+    with open(file_name, 'r') as f:  # ищем в файле очки игрока
         for string in f:
             if player in string:
                 items = string.split()
                 points = int(items[1])
             else:
-                changed_file.append(string.rstrip())          # .rstrip() удаляет символ \n
+                changed_file.append(string.rstrip())  # .rstrip() удаляет символ \n
 
     with open(file_name2, 'r') as f2:
         for string in f2:
@@ -46,13 +46,41 @@ def add_points():
 
     new_points = points + delta
 
-    if items[1] == items2[1]:
+    if items[0] == items2[0]:
         changed_file2.append(player + ' ' + str(new_points))
         f2 = open(file_name2, 'w').write("\n".join(changed_file2))
 
     changed_file.append(player + ' ' + str(new_points))  # добавляем в файл информацию об очках игрока
-    f = open(file_name, 'w').write("\n".join(changed_file))   # открываем файл для перезаписи
+    f = open(file_name, 'w').write("\n".join(changed_file))  # открываем файл для перезаписи
 
+    # далее идёт сортировка
+
+    for k in range(2):
+
+        sort_changed_file = []
+
+        if k == 0:
+            file_name = 'point.txt'
+        else:
+            file_name = 'record.txt'
+
+        with open(file_name, 'r') as f:
+            a = f.readlines()
+
+        for i in range(len(a)):
+            sort_changed_file.append(a[i].split())
+
+        sort_changed_file.sort(key=lambda i: int(i[1]))
+        sort_changed_file.reverse()
+
+        with open(file_name, 'w') as f:
+            f.seek(0)
+            for i in range(len(sort_changed_file)):
+                for j in range(2):
+                    if j == 1:
+                        f.write(str(sort_changed_file[i][j]) + '\n')
+                    else:
+                        f.write(str(sort_changed_file[i][j]) + ' ')
 if __name__ == '__main__':
     '''
     Тело главной функции

37a8455fcb83310e0388f5fcfa719bb822863b6d
Igor-chest <55881908+Igor-chest@users.noreply.github.com>
2019-10-27 18:00:23 +0500

    Update menu.py

--- menu.py
+++ menu.py
@@ -6,6 +6,8 @@
 import pygame
 import sys
 
+# функция вывода текста на экран
+
 def DrawText(text, font, surface_menu, x, y):
     textobj = font.render(text, 1, font_color)
     textrect = textobj.get_rect()
@@ -13,56 +15,63 @@ def DrawText(text, font, surface_menu, x, y):
     surface_menu.blit(textobj, textrect)
 
 pygame.font.init()
-font = pygame.font.Font(None, 72)
+fonf = pygame.font.Font(None, 72)    # шрифт
 
-bgcolor = (150, 50, 100)
-font_color = (255, 255, 153)
-highlite_color = (153, 102, 255)
+bgcolor = (150, 50, 100)     # цвет заднего фона
+font_color = (255, 255, 153)       # цвет текста
 
-surface_width = 800
-surface_height = 600
+surface_width = 800         # ширина экрана
+surface_height = 600         # высота экрана
 
-surface_menu = pygame.display.set_mode([surface_width, surface_height])
+surface_menu = pygame.display.set_mode([surface_width, surface_height])         # создание окна
 
-pygame.display.set_caption("Labirinth")
+pygame.display.set_caption("Labirinth")       # название окна
 
-surface_menu.fill(bgcolor)
+surface_menu.fill(bgcolor)         # покраска окна в цыет заднего фона
+
+# функция главного меню
 
 def main_menu():
 
     surface_menu.fill(bgcolor)
 
-    xg = (surface_width/2)-360
-    yg = (surface_height/2.5)-90
-    xsat = (surface_width/2)-360
-    ysat = (surface_height/2.5)-40
-    xst = (surface_width/2)-360
-    yst = (surface_height/2.5)+10
-    xq = (surface_width/2)-360
-    yq = (surface_height/2.5)+60
+    # координаты для каждого раздела меню
+
+    xg = (surface_width/2) - 360
+    yg = (surface_height/2.5) - 90
+    xsat = (surface_width/2) - 360
+    ysat = (surface_height/2.5) - 40
+    xst = (surface_width/2) - 360
+    yst = (surface_height/2.5) + 10
+    xq = (surface_width/2) - 360
+    yq = (surface_height/2.5) + 60
 
     font = pygame.font.Font(None, 72)
+
+    # вывод каждого раздела меню
 
     DrawText('Игра', font, surface_menu, xg, yg)
     DrawText('Настойки', font, surface_menu, xsat, ysat)
     DrawText('Статистика', font, surface_menu, xst, yst)
     DrawText('Выход', font, surface_menu, xq, yq)
 
+    # вывод круга слева от раздела 'Игра'
+
     pygame.draw.circle(surface_menu, font_color, (int(xg) - 19, int(yg) + 23), 15)
-    x_circle = int(xg) - 19
-    y_circle = int(yg) + 23
+    x_circle = int(xg) - 19     # х - координата центра круга
+    y_circle = int(yg) + 23     # у - координата центра круга
     dy_circle = y_circle
 
-    done = True
-    while done:
+    done_main = True
+    while done_main:
 
         surface_menu.blit(surface_menu, (0, 0))
         pygame.display.flip()
 
-        for i in pygame.event.get():
+        for i in pygame.event.get():    # цикл с событиями
 
-            if i.type == pygame.KEYDOWN:
-                if i.key == pygame.K_UP:
+            if i.type == pygame.KEYDOWN:     # проверка на нажатие клавишы
+                if i.key == pygame.K_UP:     # перемещение круга либо на один раздел вверх, либо в самый низ, если вверх некуда
                     if y_circle == dy_circle:
                         pygame.draw.circle(surface_menu, bgcolor, (x_circle, y_circle), 15)
                         y_circle += 150
@@ -71,7 +80,7 @@ def main_menu():
                         pygame.draw.circle(surface_menu, bgcolor, (x_circle, y_circle), 15)
                         y_circle -= 50
                         pygame.draw.circle(surface_menu, font_color, (x_circle, y_circle), 15)
-                if i.key == pygame.K_DOWN:
+                if i.key == pygame.K_DOWN:    # перемещение круга либо на один раздел вниз, либо в самый верх, если вниз некуда
                     if y_circle == dy_circle + 150:
                         pygame.draw.circle(surface_menu, bgcolor, (x_circle, y_circle), 15)
                         y_circle -= 150
@@ -80,7 +89,7 @@ def main_menu():
                         pygame.draw.circle(surface_menu, bgcolor, (x_circle, y_circle), 15)
                         y_circle += 50
                         pygame.draw.circle(surface_menu, font_color, (x_circle, y_circle), 15)
-                if i.key == pygame.K_SPACE:
+                if i.key == pygame.K_SPACE:    # вызов соответствующей функции(раздела справа от круга) при нажатии пробела
                     if y_circle == dy_circle + 100:
                         statistics()
                     if y_circle == dy_circle + 150:
@@ -88,19 +97,45 @@ def main_menu():
                     if y_circle == dy_circle + 50:
                         settings()
             if i.type == pygame.QUIT:
-                done = False
+                sys.exit()
+
+# функция раздела настроек
+
+def settings():
+
+    surface_menu.blit(surface_menu, (0, 0))
+    surface_menu.fill(bgcolor)
+
+    font = pygame.font.Font(None, 72)
+    DrawText('Настройки', font, surface_menu, (surface_width / 2) - 150, (surface_height / 2.5) - 220)      #
+
+    done_set = True
+    while done_set:
+
+        surface_menu.blit(surface_menu, (0, 0))
+        pygame.display.flip()
+
+        for iset in pygame.event.get():
+            if iset.type == pygame.KEYDOWN:
+                if iset.key == pygame.K_ESCAPE:
+                    main_menu()
+            if iset.type == pygame.QUIT:
+                sys.exit()
+
+# функция раздела статистики
 
 def statistics():
 
     surface_menu.blit(surface_menu, (0, 0))
     surface_menu.fill(bgcolor)
 
+    # вывод заголовка
     font = pygame.font.Font(None, 72)
     DrawText('Статистика', font, surface_menu, (surface_width / 2) - 150, (surface_height / 2.5) - 220)
 
     font = pygame.font.Font(None, 32)
 
-    for j in range(2):
+    for j in range(2):      # цикл, выводящий на экран текущие и рекордные очки всех игроков
         step = - 380
         bajo = -100
 
@@ -117,44 +152,23 @@ def statistics():
 
         DrawText(txt, font, surface_menu, (surface_width / 2) + step, (surface_height / 2.5) + bajo)
 
-        for i in range(len(a)):
+        for t in range(len(a)):
             bajo += 25
-            a[i] = a[i].rstrip()
-            DrawText(a[i], font, surface_menu, (surface_width / 2) + step, (surface_height / 2.5) + bajo)
+            a[t] = a[t].rstrip()
+            DrawText(a[t], font, surface_menu, (surface_width / 2) + step, (surface_height / 2.5) + bajo)
 
-    done = True
-    while done:
-
-        surface_menu.blit(surface_menu, (0, 0))
-        pygame.display.flip()
-
-        for i in pygame.event.get():
-            if i.key == pygame.K_ESCAPE:
-                main_menu()
-                break
-            if i.type == pygame.QUIT:
-                done = False
-
-def settings():
-
-    surface_menu.blit(surface_menu, (0, 0))
-    surface_menu.fill(bgcolor)
-
-    font = pygame.font.Font(None, 72)
-    DrawText('Настройки', font, surface_menu, (surface_width / 2) - 150, (surface_height / 2.5) - 220)
-
-    done = True
-    while done:
+    done_stat = True
+    while done_stat:
 
         surface_menu.blit(surface_menu, (0, 0))
         pygame.display.flip()
 
-        for i in pygame.event.get():
-            if i.key == pygame.K_ESCAPE:
-                main_menu()
-                break
-            if i.type == pygame.QUIT:
-                done = False
+        for istat in pygame.event.get():
+            if istat.type == pygame.KEYDOWN:
+                if istat.key == pygame.K_ESCAPE:
+                    main_menu()
+            if istat.type == pygame.QUIT:
+                sys.exit()
 
 main_menu()
 #statistics()

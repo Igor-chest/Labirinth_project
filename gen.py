a355ddc8cf1c6dc880eefb2290e67324262a78cc
Allekseev <55903749+Allekseev@users.noreply.github.com>
2019-11-18 22:26:58 +0500

    Update gen.py

--- gen.py
+++ gen.py
@@ -12,6 +12,7 @@ angles = [[1, 1], [1, -1], [-1, 1],
 Функция line создаёт 1 ответвление от лабиринта за вызов.
 Ответвление генерируется начиная со случайного незанятого места и заканчивается когда соеденится с уже сгенерированной 
 частью лабиринта(при первом вызове это только клетка start)
+
 на вход подаётся:
 x,y - размер лабиринта по x и по y
 zCount - кол-во не занятых клеток
@@ -26,19 +27,25 @@ def line(map,x,y,zCount,point):
     # ^ константы
     flag=True
     map[point[0]][point[1]]=ink
-    while flag:                                                 #поклеточно генерирует ответвление
-        pWays=[]
+    while flag:   #поклеточно генерирует ответвление
+        way=0
+        pWays=ways.copy()
         for i in ways:
             if map[point[0] + i[0]][point[1] + i[1]] == sym:    #пока не встретит остальной лабиринт
                 flag = False
-                pWays=[i,]
+                way=i
                 break
-            if map[point[0]+i[0]][point[1]+i[1]] in road and choose(map,x,y,point,[i[0],i[1]],sym,road,nSym):
-                pWays.append(i)
-        r=random.randint(0,pWays.__len__()-1)
-        zCount=step(map,point,pWays[r],zCount,block,ink)
-        point=[point[0]+pWays[r][0],point[1]+pWays[r][1]]
-    for i in range(x):   #делает отевтвление частью лабиринта
+        while flag:
+            if not pWays:
+                labPrint(map,x,y)
+            r=pWays[random.randint(0,len(pWays)-1)]
+            if map[point[0]+r[0]][point[1]+r[1]] in road and choose(map,x,y,point,[r[0],r[1]],sym,road,nSym):
+                way = r
+                break
+            pWays.remove(r)
+        zCount=step(map,point,way,zCount,block,ink)
+        point=[point[0]+way[0],point[1]+way[1]]
+    for i in range(x):   #делает ответвление частью лабиринта
         for j in range(y):
             if map[i][j]==4:
                 map[i][j]=1
@@ -61,6 +68,7 @@ def gen(x,y):
 
 '''
 удлиняет ответвление на 1 клетку.
+
 point - текущая позиция. [x,y]
 way - направление шага. [0,-1/1]/[-1/1,0](влево/вправо/вверх/вниз)
 zCount - кол-во незанятого места
@@ -80,6 +88,7 @@ def step(map,point,way,zCount,block,ink):
 
 '''
 проверяет есть ли путь у ветки  до лабиринта
+
 x,y - размер лабиринта по x и по y
 point - текущая позиция. [x,y]
 way - направление шага. [0,-1/1]/[-1/1,0](влево/вправо/вверх/вниз)
@@ -91,15 +100,60 @@ road - клетки через которые можно проводить но
 def choose(map,x,y,point,way,sym,road,nSym):
     tPoint=[point[0]+way[0],point[1]+way[1]]
     nMap=gen(x,y)
+    contacts=set()
+    flag = True
     for i in ways, angles:
         for j in i:
-           if (way[0] and way[0]!=j[0]) or (way[1] and way[1]!=j[1]):
-              nMap[point[0] + j[0]][point[1] + j[1]] = nSym
+            if (way[0] and way[0]!=j[0]) or (way[1] and way[1]!=j[1]):
+                nMap[point[0] + j[0]][point[1] + j[1]] = nSym
+                if not map[point[0] + j[0]][point[1] + j[1]] and flag:
+                    for k in ways, angles:
+                        for c in k:
+                            if map[point[0] + j[0]+c[0]][point[1] + j[1]+c[1]] == 9:
+                                flag = False
+                            elif map[point[0] + j[0]+c[0]][point[1] + j[1]+c[1]]==4:
+                                contacts.add((point[0] + j[0]+c[0],point[1] + j[1]+c[1]))
+
+    if flag:
+        goodCon = 0
+        #print(contacts,'!')
+        truePos=[]
+        for i in ways:
+            if map[point[0]+i[0]][point[1]+i[1]] != 5 and i != way:
+                truePos.append((point[0]+i[0],point[1]+i[1]))
+        #print(truePos,point,way)
+        for i in contacts:
+            flag=True
+            if i in truePos:
+                #print("Zashel 1")
+                break
+            n=1
+            pos = [i]
+            while n and flag:
+                n-=1
+                newPos=[]
+                for j in pos:
+                    for k in ways:
+                        if (j[0]+k[0],j[1]+k[1])in truePos:
+                            #print("Zashel 2")
+                            goodCon += 1
+                            flag=False
+                            break
+                        if map[j[0] + k[0]][j[1] + k[1]] ==4 and map[j[0] + k[0]][j[1] + k[1]] not in pos:
+                            newPos.append((j[0] + k[0], j[1] + k[1]))
+                    if not flag:
+                        break
+                pos=newPos
+        if goodCon==len(contacts):
+            #print(goodCon)
+            return True
+        #else:
+            #print("not",goodCon)
     nMap[tPoint[0]][tPoint[1]]=nSym
     possible=[tPoint]
     flag=False
     for i in possible:   #цикл проходит по массиву координат possible, в то же время добавляя в него пустые клетки с 4
-        if flag:         #сторон от текущей. Если все возможные клетки пройдены, то значит путя до лабиринта нет.
+        if flag:         #сторон от текущей. Если все возможные клетки пройдены, то значит пути до лабиринта нет.
             break
         for j in ways:
             if map[i[0]+j[0]][i[1]+j[1]]==sym:
@@ -110,11 +164,13 @@ def choose(map,x,y,point,way,sym,road,nSym):
                 possible.append([i[0]+j[0],i[1]+j[1]])
     return flag
 
-def labGen(x, y):     #MAIN
+
+
+def labGen(x,y):     #MAIN
     map = gen(x, y)
     start = [random.randint(1, x - 2), 1]
     zCount = (x - 2) * (y - 2) - 2
-    plain = zCount   # кол-во пустых клеток в пустом поле для лабиринта
+    plain = zCount   #кол-во пустых клеток в пустом поле для лабиринта
     finish = [random.randint(1, x - 2), y - 2]
     for i in range(x):
         for j in range(y):
@@ -141,53 +197,81 @@ def labGen(x, y):     #MAIN
             print('/', zCount)
             break
     line(map,x, y, zCount, finish)
-
-    map[start[0]][0] = 1
+    map[start[0]][0] = 2
     map[finish[0]][y - 1] = 2
-
     map[start[0]][start[1]] = 3
     map[finish[0]][finish[1]] = 'f'
     return map
 
-# функция загрузки стороны лабиринта из файла
+'''
+генерирует и сохраняет лабиринт в фаил
 
-def downloadGen(player):
-    changed_file_load = []
-
-    with open('save.txt', 'r') as f:  # ищем в файле очки игрока
-        for string in f:
-            if player in string:
-                items = string.split()
-                x = int(items[1])
-            else:
-                changed_file_load.append(string.rstrip())  # .rstrip() удаляет символ \n
-    return(x)
-
-# функция сохранения стороны лабиринта в файл
+х - высота
+у - ширина
+fName - имя файла, куда сохранять
+'''
+def oldSaveGen(fName,x,y):
+    map=labGen(x,y)
+    file = open(fName,'w')
+    file.write(str(y)+'\n')
+    for i in range(x):
+        for j in range(y):
+            file.write(str(map[i][j]))
+    file.close()
 
 def saveGen(player):
+
     changed_file_save = []
+
     with open('save.txt', 'r') as f:  # ищем в файле очки игрока
+
         for string in f:
+
             if player in string:
+
                 items = string.split()
+
                 x = int(items[1])
+
             else:
+
                 changed_file_save.append(string.rstrip())  # .rstrip() удаляет символ \n
 
+
+
     changed_file_save.append(player + ' ' + str(x + 2))  # добавляем в файл информацию об очках игрока
+
     open('save.txt', 'w').write("\n".join(changed_file_save)+'\n')
 
 
-'''
-for i in range(x):
-    for j in range(y):
-        if map[i][j] == 2:
-            print(green+'█'+end, end='')
-        else:
-            print(grey+'█'+end, end='')
-    print()
-map[start[0]][start[1]]='s'
-map[finish[0]][finish[1]]='f'
-'''
 
+def downloadGen(player):
+
+    changed_file_load = []
+
+
+
+    with open('save.txt', 'r') as f:  # ищем в файле очки игрока
+
+        for string in f:
+
+            if player in string:
+
+                items = string.split()
+
+                x = int(items[1])
+
+            else:
+
+                changed_file_load.append(string.rstrip())  # .rstrip() удаляет символ \n
+
+    return(x)
+
+def labPrint(map,x,y):
+    for i in range(x):
+        for j in range(y):
+            if map[i][j] in (2,5):
+                print(green+'█'+end, end='')
+            else:
+                print(grey+'█'+end, end='')
+        print()

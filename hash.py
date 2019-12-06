f5d50fd9b0002dc54185f05479f6eb533d971d7f
Igor-chest <55881908+Igor-chest@users.noreply.github.com>
2019-12-06 16:24:01 +0500

    Update hash.py

--- hash.py
+++ hash.py
@@ -1,11 +1,11 @@
 def hashFile(fileName):
-    f = open(fileName,'r')
-    codfile=[]
+    f = open(fileName, 'r')
+    codfile = []
     for line in f:
         #codline=[]
         for char in line:
             cod = str(ord(char))
-            cod='0'*(3-len(cod))+cod
+            cod = '0'*(3-len(cod))+cod
             for i in cod:
                 codfile.append(int(i))
             #codline.append(cod)
@@ -13,7 +13,7 @@ def hashFile(fileName):
     f.close()
     #print(codfile)
     fCode(codfile,False)
-    f2 = open(fileName,'w')
+    f2 = open(fileName, 'w')
     for i in codfile:
         f2.write(i)
     f2.close()
@@ -21,45 +21,45 @@ def hashFile(fileName):
 
 
 def unHash(fileName):
-    f = open(fileName,'r')
+    f = open(fileName, 'r')
     for line in f:
-        st=line
-    codfile=[]
+        st = line
+    codfile = []
     for i in st:
         codfile.append(int(i))
     fCode(codfile, True)
-    st="".join(codfile)
+    st = "".join(codfile)
     #print(st)
     f.close()
-    f2=open(fileName,'w')
-    for i in range(0,len(st),3):
+    f2 = open(fileName, 'w')
+    for i in range(0, len(st), 3):
         #print(st[i]+st[i+1]+st[i+2])
         #print(chr(int(st[i]+st[i+1]+st[i+2])))
         f2.write(chr(int(st[i]+st[i+1]+st[i+2])))
     f2.close()
 
 
-def fCode(arrNumber,flag):
+def fCode(arrNumber, flag):
     #print(arrNumber)
-    fib2=[1,1]
-    pos=0
-    i=0
-    while pos<len(arrNumber):
-        fib2[i%2]+=fib2[not i%2]
-        strNum=str(fib2[i%2])
-        i+=1
+    fib2 = [1, 1]
+    pos = 0
+    i = 0
+    while pos < len(arrNumber):
+        fib2[i % 2] += fib2[not i % 2]
+        strNum = str(fib2[i % 2])
+        i += 1
         #print(strNum)
         for j in strNum:
             if pos >= len(arrNumber):
                 break
             #print(arrNumber[pos])
-            arrNumber[pos]=str(((int(arrNumber[pos])+(flag*(-1)+1*(not flag))*int(j)))%10)
+            arrNumber[pos] = str(((int(arrNumber[pos]) + (flag * (-1) + 1 * (not flag)) * int(j))) % 10)
             #print(arrNumber)
-            pos+=1
+            pos += 1
     #print(arrNumber)
 
 
 for i in ('point.txt', 'record.txt', 'save.txt', 'password.txt'):
     unHash(i)
     #hashFile(i)
-#hashFile('test.txt')
+#hashFile('test.txt')')
